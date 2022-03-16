dffe4778c930e834d70a5eae6d6c1d47bc01fc9f
Nils-Helge Garli Hegvik <nghe@equinor.com>
2022-03-16 14:10:46 +0100

    Update default prod version to 1.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="omnia-timeseries-api",
-    version="1.1.0",
+    version="1.1.1",
     author="Equinor Omnia Industrial IoT Team",
     description="Official Python SDK for the Omnia Timeseries API",
     long_description=long_description,
