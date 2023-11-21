93523cd0111b1028e7e4dce4b7c589739e727f0d
Torgeir Ladstein Waagbø <TLW@equinor.com>
2023-11-21 12:20:54 +0100

    convert to github actions

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
-from setuptools import find_packages, setup
+# from setuptools import find_packages, setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+# with open("README.md", "r", encoding="utf-8") as fh:
+    # long_description = fh.read()
 
-setup(
-    name="omnia-timeseries-api",
-    version="1.2.0",
+# setup()
+#     name="omnia-timeseries-api",
+#     version="1.2.0",
 
-    author="Equinor Omnia Industrial IoT Team",
-    description="Official Python SDK for the Omnia Timeseries API",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/equinor/omnia-timeseries-python",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.8',
-    install_requires=[
-        "azure-identity>=1.10.0",
-        "pytest",
-        "requests_mock",
-        "cryptography>=39.0.1",
-        "pyjwt>=2.4.0", 
-        "opentelemetry-instrumentation-requests>=0.31b0"
-    ]
-)
+#     author="Equinor Omnia Industrial IoT Team",
+#     description="Official Python SDK for the Omnia Timeseries API",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/equinor/omnia-timeseries-python",
+    # packages=find_packages(),
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#         "Operating System :: OS Independent",
+#     ],
+#     python_requires='>=3.8',
+#     install_requires=[
+#         "azure-identity>=1.10.0",
+#         # "pytest",
+#         "requests_mock",
+#         "cryptography>=39.0.1",
+#         "pyjwt>=2.4.0", 
+#         "opentelemetry-instrumentation-requests>=0.31b0"
+#     ]
+# )
